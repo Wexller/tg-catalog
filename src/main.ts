9ae16f52edbec572de00b1697848eeedaaf984d5
Maxim Davydov <m.davydov@magnus-tech.ru>
2023-10-01 15:09:18 +0300

    Move styles

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
-import './style.css';
 import App from './App.vue';
 import VueTelegram from 'vue-tg';
+import './assets/styles/main.css';
+import './assets/styles/tg.css';
 
 const app = createApp(App);
 app.use(VueTelegram);
