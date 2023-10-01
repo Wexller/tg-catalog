45250718bf7b764011d49dc56934ec6d642d0d18
Maxim Davydov <m.davydov@magnus-tech.ru>
2023-10-01 11:55:33 +0300

    Add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  root: './',
+  base: '/tg-catalog/',
+  build: {
+    outDir: './build',
+  },
 });
